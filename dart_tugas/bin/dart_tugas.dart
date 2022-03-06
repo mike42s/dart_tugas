84b05935479b168801e6acbc5f82efe8cb651c39
Ryan Jonathan S <94353686+mike42s@users.noreply.github.com>
2022-03-06 12:31:28 +0700

    Versi Final No 1

--- dart_tugas/bin/dart_tugas.dart
+++ dart_tugas/bin/dart_tugas.dart
@@ -9,48 +9,63 @@ int fungsi_pohon(int input) {
         for (int k = 0; k < (input - i - 1) / 2; k++) {
           stdout.write("*");
         }
-        for (int j = 0; j < i; j++) {
-          stdout.write((i).toString());
+        for (int j = 0; j < i * 2 - 1; j++) {
+          if (i % 2 == 0) {
+            if (j % 2 == 0) {
+              stdout.write((i).toString());
+            } else {
+              stdout.write(" ");
+            }
+          } else {
+            if (j % 2 == 1 || i == 1) {
+              stdout.write((i).toString());
+            } else {
+              stdout.write(" ");
+            }
+          }
         }
         for (int k = 0; k < (input - i - 1) / 2; k++) {
           stdout.write("*");
         }
+      } else if ((input + 1) / 2 == i) {
+        for (int j = 0; j < input; j++) {
+          if (j % 2 == 0) {
+            stdout.write((i).toString());
+          } else {
+            stdout.write(" ");
+          }
+        }
       } else {
-        for (int k = input - i - 1; k >= 0; k--) {
+        for (int k = input - i - 1; k <= 0; k++) {
           stdout.write("*");
         }
-        for (int j = input - i; j >= 0; j--) {
-          stdout.write(i);
+        for (int j = (input + 1 - i) * 2 - 1; j > 0; j--) {
+          if (i % 2 == 0) {
+            if (j % 2 == 0) {
+              stdout.write((i).toString());
+            } else {
+              stdout.write(" ");
+            }
+          } else {
+            if (j % 2 == 1) {
+              stdout.write((i).toString());
+            } else {
+              stdout.write(" ");
+            }
+          }
         }
-        for (int k = input - i - 1; k >= 0; k--) {
+        for (int k = (input - i - 1); k <= 0; k++) {
           stdout.write("*");
         }
       }
-    }
-    stdout.write("\n");
-  }
 
-  // if (input % 2 != 0) {
-  //     if (i < input / 2) {
-  //       for (int k = 0; k <= (input - i - 1) / 2; k++) {
-  //         stdout.write("*");
-  //       }
-  //       for (int j = -1; j < i; j++) {
-  //         stdout.write((i + 1).toString());
-  //       }
-  //       for (int k = 1; k <= (input - i - 1) / 2; k++) {
-  //         stdout.write("*");
-  //       }
-  //     } else {
-  //       for (int j = input - i; j > 0; j--) {
-  //         stdout.write(i + 1);
-  //         // stdout.write("*");
-  //       }
-  //     }
-  //   }
+      stdout.write("\n");
+    }
+  }
   return 0;
 }
 
+//int fungsipohon2(int input) {}
 void main(List<String> arguments) {
   stdout.write("Masukkan Kata : ");
   String? Input = stdin.readLineSync();
